c1e743179b068bb2e9a7a88fc0778b82eb38a5a8
BrutalCoding <dhbreedeveld@gmail.com>
2016-08-27 04:55:40 +0200

    IV Calc final layout added

--- PokeBro/PokeBro/IVCalc.cs
+++ PokeBro/PokeBro/IVCalc.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace PokeBro
@@ -12,6 +13,9 @@ namespace PokeBro
             { "Aero"}
         };
 
+        public Entry inputCP { get; set; }
+        public Entry inputHP { get; set; }
+        Picker picker;
         public IVCalc()
         {
             var layout = new StackLayout { Padding = new Thickness(15,15) };
@@ -19,10 +23,11 @@ namespace PokeBro
             var labelPicker = new Label { Text = "Choose your monster", TextColor = Color.Black, FontSize = 20};
 
             //Go through each Pokemon and add them to the list.
-            Picker picker = new Picker
+            picker = new Picker
             {
                 Title = "...",
-                VerticalOptions = LayoutOptions.Start
+                VerticalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 0, 0, 50)
             };
 
             foreach (string pokemon in listOfPokemon)
@@ -31,13 +36,28 @@ namespace PokeBro
             }
 
             var labelInputCP = new Label { Text = "CP", TextColor = Color.Black, FontSize = 14 };
-            var inputCP = new Entry { Text = "", Keyboard = Keyboard.Numeric };
+            inputCP = new Entry { Text = "", Keyboard = Keyboard.Numeric };
 
             var labelInputHP = new Label { Text = "HP", TextColor = Color.Black, FontSize = 14 };
-            var inputHP = new Entry { Text = "", Keyboard = Keyboard.Numeric };
+            inputHP = new Entry { Text = "", Keyboard = Keyboard.Numeric };
 
             var labelInputStartdust = new Label { Text = "Stardust", TextColor = Color.Black, FontSize = 14 };
             var inputStartdust = new Entry { Text = "", Keyboard = Keyboard.Numeric };
+
+            var labelSwitch = new Label { Text = "I have power upped this monster", TextColor = Color.Black, FontSize = 14 };
+            Switch switcher = new Switch { HorizontalOptions = LayoutOptions.Start, VerticalOptions = LayoutOptions.Start};
+            switcher.Toggled += switcher_Toggled;
+
+            var btnCalculate = new Button {
+                Text = "Calculate",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                BackgroundColor = Color.FromHex("#42A5F5"),
+                TextColor = Color.White,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            btnCalculate.Clicked += OnButtonClicked;
 
             layout.Children.Add(labelPicker);
             layout.Children.Add(picker);
@@ -47,6 +67,33 @@ namespace PokeBro
             layout.Children.Add(inputHP);
             layout.Children.Add(labelInputStartdust);
             layout.Children.Add(inputStartdust);
+            layout.Children.Add(labelSwitch);
+            layout.Children.Add(switcher);
+            layout.Children.Add(btnCalculate);
+        }
+
+        void switcher_Toggled(object sender, ToggledEventArgs e)
+        {
+            //The chosen pokemon has been power upped, which will give a different result
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            //Formula from this reddit thread: https://www.reddit.com/r/TheSilphRoad/comments/4t7r4d/exact_pokemon_cp_formula/
+
+            double baseAttack = 0;
+            double baseDefense = 0;
+            double baseStamina = Convert.ToDouble(inputHP.Text) * 2;
+            switch (listOfPokemon[picker.SelectedIndex])
+            {
+                case "Abra":
+                    baseAttack = 110;
+                    baseDefense = 76;
+                    break;
+            }
+            //double IV = 0;
+            //double MaxCP = 0.1 * ((baseAttack + IV) * (baseDefense + IV) ^ 0.5 * (baseStamina + IV) ^ 0.5 * (CpM + ACpM) ^ 2);
+
         }
     }
 }
